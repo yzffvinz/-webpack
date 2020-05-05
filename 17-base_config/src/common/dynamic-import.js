97049f7409109e91fa728dd3c1028a7436a53bdc
yzffvinz <1178043794@qq.com>
2020-05-05 10:45:53 +0800

    empty initial

--- 17-base_config/src/common/dynamic-import.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import React from 'react';
-
-// eslint-disable-next-line react/jsx-filename-extension
-export default () => <div>dynamic-import</div>;
--- Charpert4/builder-webpack/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "parser": "babel-eslint",
-    "extends": "airbnb-base",
-    "env": {
-        "browser": true,
-        "node": true
-    }
-};
